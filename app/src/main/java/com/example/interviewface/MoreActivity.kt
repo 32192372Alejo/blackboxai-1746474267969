0eb566afc147b5c5091983899a3ccedda69e1393
32192372Alejo <alejo290816@gmail.com>
2025-05-07 15:45:59 -0500

    Add app/src/main/java/com/example/interviewface/MoreActivity.kt 1746650600494

--- app/src/main/java/com/example/interviewface/MoreActivity.kt
+++ app/src/main/java/com/example/interviewface/MoreActivity.kt
@@ -20,25 +20,33 @@ class MoreActivity : AppCompatActivity() {
     private fun setupBottomNavigation() {
         binding.bottomNavigation.selectedItemId = R.id.navigation_more
         binding.bottomNavigation.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.navigation_home -> {
-                    startActivity(Intent(this, HomeActivity::class.java))
-                    true
+            if (item.itemId == binding.bottomNavigation.selectedItemId) {
+                true
+            } else {
+                when (item.itemId) {
+                    R.id.navigation_home -> {
+                        startActivity(Intent(this, HomeActivity::class.java))
+                        finish()
+                        true
+                    }
+                    R.id.navigation_interviews -> {
+                        startActivity(Intent(this, InterviewsActivity::class.java))
+                        finish()
+                        true
+                    }
+                    R.id.navigation_comments -> {
+                        startActivity(Intent(this, CommentsActivity::class.java))
+                        finish()
+                        true
+                    }
+                    R.id.navigation_profile -> {
+                        startActivity(Intent(this, ProfileActivity::class.java))
+                        finish()
+                        true
+                    }
+                    R.id.navigation_more -> true
+                    else -> false
                 }
-                R.id.navigation_interviews -> {
-                    startActivity(Intent(this, InterviewsActivity::class.java))
-                    true
-                }
-                R.id.navigation_comments -> {
-                    startActivity(Intent(this, CommentsActivity::class.java))
-                    true
-                }
-                R.id.navigation_profile -> {
-                    startActivity(Intent(this, ProfileActivity::class.java))
-                    true
-                }
-                R.id.navigation_more -> true
-                else -> false
             }
         }
     }
