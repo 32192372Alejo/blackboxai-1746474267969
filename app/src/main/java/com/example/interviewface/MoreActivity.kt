f6b04d3b1452eea3fa28a05d1a7991eeea3bd3b6
32192372Alejo <alejo290816@gmail.com>
2025-05-07 15:40:19 -0500

    Add app/src/main/java/com/example/interviewface/MoreActivity.kt 1746650260745

---
+++ app/src/main/java/com/example/interviewface/MoreActivity.kt
@@ -0,0 +1,45 @@
+package com.example.interviewface
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.interviewface.databinding.ActivityBaseScreenBinding
+
+class MoreActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBaseScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBaseScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupBottomNavigation()
+    }
+
+    private fun setupBottomNavigation() {
+        binding.bottomNavigation.selectedItemId = R.id.navigation_more
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    startActivity(Intent(this, HomeActivity::class.java))
+                    true
+                }
+                R.id.navigation_interviews -> {
+                    startActivity(Intent(this, InterviewsActivity::class.java))
+                    true
+                }
+                R.id.navigation_comments -> {
+                    startActivity(Intent(this, CommentsActivity::class.java))
+                    true
+                }
+                R.id.navigation_profile -> {
+                    startActivity(Intent(this, ProfileActivity::class.java))
+                    true
+                }
+                R.id.navigation_more -> true
+                else -> false
+            }
+        }
+    }
+}
